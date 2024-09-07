93e4bb3a7869c8f8b03ccf227422f062ae83728f
gabrielyvg <gabrielyvg@outlook.com>
2024-09-07 12:40:54 -0300

    feat: creating the base structure of the project

---
+++ src/weather/weather.controller.ts
@@ -0,0 +1,7 @@
+import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
+import { ResultadoDto } from "src/dto/resultado.dto";
+
+@Controller('weather')
+export class WeatherController {
+  
+}
