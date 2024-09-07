c71085d1664dea9960047e7ad8f8ccd0f43ad6f0
gabrielyvg <gabrielyvg@outlook.com>
2024-09-07 16:27:50 -0300

    feat: adding error messages

--- src/weather/dto/weather.dto.ts
+++ src/weather/dto/weather.dto.ts
@@ -1,15 +1,15 @@
 import { IsNotEmpty, IsNumber } from "class-validator";
 
 export class WeatherDto {
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Latitude não deve ser vazio' })
   @IsNumber({}, { message: 'Latitude deve ser um número' })
   lat: number;
 
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Longitude não deve ser vazio' })
   @IsNumber({}, { message: 'Longitude deve ser um número' })
   lon: number;
 
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Humidade não deve ser vazio' })
   @IsNumber({}, { message: 'Humidade deve ser um número' })
   humidity: number;
 }
