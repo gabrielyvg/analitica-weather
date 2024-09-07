6e8d37e72c6cec7f25c8e14641af76bb270582bf
gabrielyvg <gabrielyvg@outlook.com>
2024-09-07 16:36:42 -0300

    adding the swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -25,5 +26,15 @@ async function bootstrap() {
       });
     },
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Weather API')
+    .setDescription('The Weather API description')
+    .setVersion('1.0')
+    .addTag('weather')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 }
 bootstrap();
